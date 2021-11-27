cdfb522d86cb3b6d0e211934e6c4d97a02e1c3a1
BenetManzanaresSalor <benetmanzanares@gmail.com>
2021-11-27 16:58:31 +0100

    UserAgent settings.xml reading and dataset splitting

--- IMAS_WORK_T01/src/main/java/urv/imas/agents/UserAgent.java
+++ IMAS_WORK_T01/src/main/java/urv/imas/agents/UserAgent.java
@@ -16,72 +16,121 @@ import java.util.Enumeration;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
+import javax.xml.parsers.*;
+import java.io.File;
+import org.w3c.dom.*;
+
 /**
- * This agent implements a simple Ping Agent that registers itself with the DF and
- * then waits for ACLMessages.
- * If  a REQUEST message is received containing the string "ping" within the content
- * then it replies with an INFORM message whose content will be the string "pong".
- *
- * @author Tiziana Trucco - CSELT S.p.A.
- * @version  $Date: 2010-04-08 13:08:55 +0200 (gio, 08 apr 2010) $ $Revision: 6297 $
+ * This agent implements a simple Uset Agent that loads settings, datasets and starts training and testing. *
+ * @author Team 1
  */
 public class UserAgent extends Agent
 {
-    private int nResponders;
-    private String CoordName = "coordinator";
-    private AID CoordinatorAID;
+    // Predefined settings
+    private static String ResourcesFolderPath = "./src/main/resources";
+    private static String SettingsFileName = "settings.xml";
+    private static String CoordName = "coordinator";
 
+    // Settings
+    private String DatasetFileName;
+    private int NumTrainingInstances;
+    private int NumValidationInstances;
+    private int NumTestInstances;
+    private int NumTestAttributes;
+
+    // Variables
+    private AID CoordinatorAID;
+    private Instances Dataset;
+    private Instances TrainDataset;
+    private Instances ValidationDataset;
+    private Instances TestDataset;
+
+
+    ///////////////////////////////////////////////////////////////// Auxiliar methods /////////////////////////////////////////////////////////////////
     public void showMessage(String mss) {
         System.out.println(getLocalName()+" -> "+mss);
     }
 
-    //Nou comportament que estén la part iniciadora d’un Query
+    ///////////////////////////////////////////////////////////////// Initialization /////////////////////////////////////////////////////////////////
+    protected void setup() {
+        // Read creation arguments
+        readArguments();
+
+        // Read settings XML file
+        readSettings();
+
+        // Read dataset specified
+        readDataset();
+
+        // Start comunication with coordinator
+        CoordinatorAID = new AID((String) CoordName, AID.ISLOCALNAME);
+        ACLMessage msg = new ACLMessage(ACLMessage.INFORM); //Instanciació del missatge
+        msg.addReceiver(CoordinatorAID);    //Afegim un receptor (suposem que el seu AID ja l’havíem trobat abans
+        msg.setSender(getAID());    //Ens posem a nosaltres com a emissor
+        msg.setContent("I am USER");    //Indiquem el contingut (String sense estructura)
+
+        // Create initializaiton behaviour
+        QueryInitiator bh1 = new QueryInitiator(this, msg);
+        addBehaviour(bh1);
+    }
+
+    protected void readArguments(){
+        Object[] args = getArguments();
+        if (args != null && args.length > 0) {} else{}  // TODO
+    }
+
+    protected void readSettings(){
+        showMessage("Reading settings...");
+        File file = new File(ResourcesFolderPath+"/"+SettingsFileName);
+        try {
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(file);
+
+            DatasetFileName = document.getElementsByTagName("dataset_filename").item(0).getTextContent();
+            NumTrainingInstances = Integer.parseInt(document.getElementsByTagName("num_training_instances").item(0).getTextContent());
+            NumValidationInstances = Integer.parseInt(document.getElementsByTagName("num_validation_instances").item(0).getTextContent());
+            NumTestInstances = Integer.parseInt(document.getElementsByTagName("num_test_instances").item(0).getTextContent());
+            NumTestAttributes = Integer.parseInt(document.getElementsByTagName("num_training_attributes").item(0).getTextContent());
+        }catch(Exception e){showMessage(e.getMessage());}
+    }
+
+    protected void readDataset(){
+        showMessage("Reading dataset...");
+        try {
+            // Read dataset
+            DataSource source = new DataSource(ResourcesFolderPath+"/"+DatasetFileName);
+            Dataset = source.getDataSet();
+            if (Dataset.classIndex() == -1) {   // Set class index
+                Dataset.setClassIndex(Dataset.numAttributes() - 1);
+            }
+            showMessage(Dataset.toSummaryString());
+
+            // Split dataset (https://www.programcreek.com/java-api-examples/?api=weka.filters.Filter Example 3)
+            int iniIdx = 0;
+            int endIdx = NumTrainingInstances;
+            TrainDataset = new Instances(Dataset, iniIdx, endIdx);
+            iniIdx = endIdx;
+            endIdx += NumValidationInstances;
+            ValidationDataset = new Instances(Dataset, iniIdx, endIdx);
+            iniIdx = endIdx;
+            endIdx += NumTestInstances;
+            TestDataset = new Instances(Dataset, iniIdx, endIdx);
+        }catch(Exception e){showMessage(e.getMessage());}
+    }
+
+    // Initialization behaviour
     class QueryInitiator extends AchieveREInitiator
     {
         public QueryInitiator (Agent myAgent, ACLMessage msg)
         {
             super(myAgent, msg);
         }
-        //Mètodes que caldrà implementar amb les accions pertinents
+
         protected void handleInform (ACLMessage msg)
         {
             showMessage("'" + msg.getSender().getLocalName()+"' sent: \""+msg.getContent()+"\"");
         }
-
-    }
-
-    protected void setup() {
-        // Read name of coordinator as argument (NOT ACTIVE NOW)
-        Object[] args = getArguments();
-        if (args != null && args.length > 0) {}else{}
-
-        CoordinatorAID = new AID((String) CoordName, AID.ISLOCALNAME);
-
-        //Instanciació del missatge
-        ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-        //Afegim un receptor (suposem que el seu AID ja l’havíem trobat abans
-        msg.addReceiver(CoordinatorAID);
-        //Ens posem a nosaltres com a emissor
-        msg.setSender(getAID());
-        //Indiquem el contingut (String sense estructura)
-        msg.setContent("I am USER");
-
-        QueryInitiator bh1 = new QueryInitiator(this, msg);
-        addBehaviour(bh1);
-        readDataset();
-    }
-
-    protected void readDataset()
-    {
-        try {
-            showMessage("Reading dataset...");
-            DataSource source = new DataSource("./src/main/resources/audit_risk.arff");
-            Instances data = source.getDataSet();
-            if (data.classIndex() == -1) {
-                data.setClassIndex(data.numAttributes() - 1);
-            }
-            showMessage(data.toSummaryString());
-        }catch(Exception e){showMessage(e.getMessage());}
     }
 }
 
